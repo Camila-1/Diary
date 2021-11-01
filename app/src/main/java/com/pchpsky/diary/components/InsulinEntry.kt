4493060512a9b7cbd23d28074f147a484360b649
Camila-1 <camilaabalova@gmail.com>
2021-11-01 14:00:22 +0200

    remove card view from insulin entry layout

--- app/src/main/java/com/pchpsky/diary/components/InsulinEntry.kt
+++ app/src/main/java/com/pchpsky/diary/components/InsulinEntry.kt
@@ -8,32 +8,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pchpsky.diary.datasource.network.model.Insulin
 import com.pchpsky.diary.theme.DiaryTheme
 
 @Composable
 fun InsulinEntry(insulin: Insulin) {
-    Card(
-        backgroundColor = DiaryTheme.colors.cardBackground,
-        modifier = Modifier
-            .wrapContentSize(Alignment.Center),
-        shape = DiaryTheme.shapes.roundedCard
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(20.dp)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(12.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            InsulinColorCircle(
-                color = Color(parseColor(insulin.color)),
-                modifier = Modifier
-                    .align(Alignment.Bottom)
-            )
-            Text(
-                text = insulin.name,
-                color = DiaryTheme.colors.text,
-                modifier = Modifier.align(Alignment.CenterVertically)
-            )
-        }
+        InsulinColorCircle(
+            color = Color(parseColor(insulin.color)),
+            modifier = Modifier
+                .align(Alignment.Bottom)
+        )
+        Text(
+            text = insulin.name,
+            color = DiaryTheme.colors.text,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
     }
 }
